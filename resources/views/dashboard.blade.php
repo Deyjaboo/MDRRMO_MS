69f444db173b4c0a6f5eb298bf567400a88513a1
Ella <bandomariella0001@gmail.com>
2022-07-28 22:51:00 +0800

    Dashboard table and card panel

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,10 +7,16 @@
     
     <!----======== CSS ======== -->
     <link rel="stylesheet" href="css/dashboard.css">
-    <link rel="stylesheet" href="css/styles.css">
+   
+    <link rel="stylesheet" href="css/styles.css"> 
+
     <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,200,400,500,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+ 
     <title>Admin Dashboad</title>
 </head>
+
 <body>
     <nav class="sidebar close">
         <header>
@@ -32,7 +38,7 @@
             <div class="menu">
 
 
-                <ul class="menu-links">
+                <ul class="menu-link">
                     <li class="nav-link">
                         <a href="dashboard"method="GET">
                             <i class='bx bx-home-alt icon' ></i>
@@ -63,31 +69,81 @@
                             <span class="text nav-text">Feedback</span>
                         </a>
                     </li> -->
+
                     <form method="POST" action="{{ route('logout') }}">
                             @csrf
 
                       
                     <li class="nav-link">
-                   
                     <a href="route('logout')"
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                         <i class='bx bx-log-out icon' ></i>
                         <span class="text nav-text">Logout</span>
                     </a>
+                     </li>
                      
-                </li>
                 </form>
                </ul>
             </div>
-        </div>
-        
+        </div>      
     </nav>
+
+<section id="home">
+<div class="wrapper">
+    <div class="form_container">
+
+
+    </div>
+        </div>  
+
+</section>
+
+
 
     <section class="home">
         <div class="text">Online Document Request System</div>
-        <div class="add">Dashboard</div>
-        <div class="card-body">
+        <h5 class="add">Dashboard</h5>
+        <div class="add">
+        <div class="main-section">
+		<div class="dashbord">
+			<div class="icon-section">
+				<i class="fa fa-users" aria-hidden="true"></i><br>
+				<small>Enrolled Students</small>
+				<p>1156</p>
+			</div>
+			<div class="detail-section">
+				<a href="#">More Info </a>
+			</div>
+		</div>
+		<div class="dashbord dashbord-red">
+			<div class="icon-section">
+				<i class="fa fa-file" aria-hidden="true"></i><br>
+				<small>New Request</small>
+				<p> 26</p>
+			</div>
+			<div class="detail-section">
+				<a href="#">More Info </a>
+			</div>
+		</div>
+		<div class="dashbord dashbord-orange">
+			<div class="icon-section">
+				<i class="fa fa-bell" aria-hidden="true"></i><br>
+				<small>Unclaimed Process Document</small>
+             
+				<p>19 New</p>
+			</div>
+			<div class="detail-section">
+				<a href="#">More Info </a>
+			</div>
+		</div>
+
+	</div>
+
+        </div>
+    <div class="wrapper">
+    <div class="form_container">
+                        <div class="card-body">
                                 <table id="datatablesSimple">
                                     <thead>
                                         <tr>
@@ -252,11 +308,8 @@
 
                                     </tbody>
                                 </table>
-                            </div>
-                        </div>
+
     </section>
-
-
 
     <script>
         const body = document.querySelector('body'),
@@ -277,8 +330,9 @@ searchBtn.addEventListener("click" , () =>{
 
 
     </script>
-         <script src="js/scripts.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>   <!-- grap link-->
+
+        <script src="js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>   grap link
         <script src="assets/demo/chart-bar-demo.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>  <!-- gride line table-->
         <script src="js/datatables-simple-demo.js"></script>
